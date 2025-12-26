e6536e67d91e7e3c43c89da3323eb1cc1cf2929d
chocolateimage <chocolateimage@protonmail.com>
2025-12-26 14:19:25 +0100

    Fix inverted room peek position

--- LinuxWindowDancePlugin/CustomWindowLinux.cs
+++ LinuxWindowDancePlugin/CustomWindowLinux.cs
@@ -8,4 +8,14 @@ public class CustomWindowLinux : CustomWindow
     {
         Native.SetWindowTextureSize(Window.WindowPtr, renderTexture.width, renderTexture.height);
     }
+
+    public override Vector2Int GetPosition()
+    {
+        var pos = Window.GetPosition();
+        var size = Window.GetSize();
+        PlatformHelperLinux platformHelperLinux = platformHelper as PlatformHelperLinux;
+        int y = platformHelperLinux.WindowDanceResolution.y;
+        Vector2Int point = platformHelperLinux.GetBottomLeftDesktopPoint();
+        return new Vector2Int(pos.x - point.x, y - (pos.y + size.height - point.y));
+    }
 }
